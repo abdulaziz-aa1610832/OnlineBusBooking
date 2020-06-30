2ffdb05c91193e3fdd7fbc6a6878092f96bfa3c4
AbdullahAlSolaiman <a.alsolaiman@graduate.utm.my>
2020-06-30 16:42:47 +0800

    added gerRoutes Info, its respective json file, and its html

--- Js_by_Abdullah/getUserName.js
+++ Js_by_Abdullah/getUserName.js
@@ -1,8 +1,10 @@
 // listen to the click, once clicked, perform getUserName function.
 
-document.getElementById("buttonID")
+document.getElementById("getUser")
 .addEventListener('click', getUsername);
 
+document.getElementById("getRoute")
+.addEventListener('click', getRoutesInfo);
 
 
 
@@ -20,16 +22,40 @@ function getUsername(){
             output += `
 
             <ul>
-            <li>UserId: ${user.id}<\li>
-            <li>UserName: ${user.name}<\li>
-            </ul>
-             
+            <li>UserId: ${user.id}</li>
+            <li>UserName: ${user.name}</li>
+            </ul>  
             `;
-
-            document.getElementById("output").innerHTML = output;
+            document.getElementById("outputUsers").innerHTML = output;
         })
+    })  
+}
 
-    })
-    
 
+function getRoutesInfo(){
+
+    fetch("routes.json")
+    .then((response_from_api) => response_from_api.json() )
+    .then((data_from_json) => {
+
+        //console.log(data_from_json)
+        let output = '<h2>Routes I got </h2>';
+        
+
+        // we loop through each route in the json file
+        data_from_json.forEach(function(route){
+
+            output += `
+            Route No. ${route.id}
+            <ul>
+            <li>From: ${route.origin}</li>
+            <li>To: ${route.destination}</li>
+            <li>Date: ${route.date}</li>
+            <li>Capacity: ${route.capacity}</li>
+            
+            </ul>  
+            `;
+            document.getElementById("outputRoutes").innerHTML = output;
+        })
+    })  
 }
