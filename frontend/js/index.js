46da6d4cfd27bc35f93ede650d71496888411a51
Tareq Al-Ahdal <TareqOAlAhdal@gmail.com>
2020-07-03 02:54:23 +0800

    Added "Full Name"text field for sign up

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,9 @@
 function changeToSignUp() {
     document.forms[0].action="";
     document.forms[0].childNodes[1].innerHTML="Sign Up";
-    var newEmailForm = '<div class="input-container"><input type="email" name="email" id="email" required><label>Email</label></div>';
-    document.getElementById("signUpEmail").innerHTML= newEmailForm;
+    var newFormContent = '<div class="input-container"><input type="text" name="fullName" id="fullName" required><label>Full Name</label></div>';
+    document.getElementById("signUpContent").innerHTML= newFormContent;
+    newFormContent += '<div class="input-container"><input type="email" name="email" id="email" required><label>Email</label></div>';
     document.forms[0].id="signup";
     newSubmitBtn = '<input type="submit" class="btn" value="Submit"><div id="haveAnAccount"><a href="index.html">Have an account?</a></div>';
     document.getElementById("buttons").innerHTML=newSubmitBtn;
