25ca2aabb8faeba379e7f6ba01466df04ef751c6
Tareq Al-Ahdal <TareqOAlAhdal@gmail.com>
2020-07-04 00:02:14 +0800

    added sorting feature with the skeleton of the function

---
+++ frontend/js/dashboard.js
@@ -0,0 +1,8 @@
+function sortTicket() {
+    var sortingMethod = document.getElementById("sorting").value;
+    if (sortingMethod == "price-low-to-high"){
+        // do something here
+    } else if (sortingMethod == "price-high-to-low") {
+        // do something here 
+    }
+}
