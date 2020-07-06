12c9dc8bf0654aa687edbbbe3110c7f611290c60
Saeed Hashem <saeedsaeed00093@gmail.com>
2020-07-06 17:00:43 +0800

    doLogout action added and tested

--- backend/api.php
+++ backend/api.php
@@ -291,5 +291,25 @@ switch ($action) {
         }
 
         break;
+    
+    case "doLogout":
+        $_SESSION = array();
+        session_unset();
+        //setcookie(session_name(), '', time() - 42000);
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+        session_destroy();
+        var_dump($_SESSION);
+        break;
 
 }
