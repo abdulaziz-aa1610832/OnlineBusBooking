afd99124b57c55192321f84885f71f4693567d44
Saeed Hashem <saeedsaeed00093@gmail.com>
2020-07-07 19:17:44 +0800

    added CREATE and USE DATABASE

--- backend/bus_online_v06.sql
+++ backend/bus_online_v06.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `bus_online`
 --
-
+CREATE DATABASE IF NOT EXISTS `bus_online` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bus_online`;
 -- --------------------------------------------------------
 
 --
