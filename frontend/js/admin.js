e449ac698f9d58bce035ba594d53ad9ad227a9f0
AbdullahAlSolaiman <a.alsolaiman@graduate.utm.my>
2020-07-08 03:13:40 +0800

    adde sekelton of getFeeback in admin.js temporarily

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -91,7 +91,7 @@ function doLogout(theEvent){
 
 function getAllBookings(){
 
-
+    
     fetch('http://127.0.0.1/OnlineBusBooking/backend/api.php', {
         method:'POST',
         headers: {
@@ -267,4 +267,85 @@ function deleteBooking(){
         }
 
     })
+}
+
+
+function getFeedback(){
+
+     //action=getFeedback&data={}
+    // {"success":"true", "data":{1:{json object of message with id one}, 2:{} , 3:{} }}
+
+    console.log("get all feebacks start..")
+    fetch('http://127.0.0.1/OnlineBusBooking/backend/api.php', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+        body: `action=getFeedbacks&data={}`
+    })
+    .then((response_from_api) => response_from_api.json())
+    .then(data_in_json => {
+        if(data_in_json.success){  
+            console.log("success is true..")
+
+            let divToInsert = `<table class="content-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Booking id</th>
+                <th>Date</th>
+                <th>Name</th>
+                <th>Origin</th>
+                <th>Destination</th>   
+                <th>Payment</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+  `;
+            for (i in data_in_json.data) {
+                console.log(data_in_json[i]);
+                if (data_in_json.data[i].status == 1) {
+                    data_in_json.data[i].status = "confirmed";
+                    divToInsert += `<tr class="active-row">
+                    <td><input type="radio" id="${data_in_json.data[i].bookingid}" name="booking" value="${data_in_json.data[i].bookingid}"></td>
+                    <td>${data_in_json.data[i].bookingid}</td>
+                    <td>${data_in_json.data[i].date}</td>
+                    <td>${data_in_json.data[i].name}</td>
+                    <td>${data_in_json.data[i].origin}</td>
+                    <td>${data_in_json.data[i].destination}</td>
+                    <td>${data_in_json.data[i].payment}</td>
+                    <td>${data_in_json.data[i].status}</td>
+                </tr>`;
+            }
+                else{
+                    data_in_json.data[i].status = "pending";
+                    divToInsert += `<tr>
+                    <td><input type="radio" id="${data_in_json.data[i].bookingid}" name="booking" value="${data_in_json.data[i].bookingid}"></td>
+                    <td>${data_in_json.data[i].bookingid}</td>
+                    <td>${data_in_json.data[i].date}</td>
+                    <td>${data_in_json.data[i].name}</td>
+                    <td>${data_in_json.data[i].origin}</td>
+                    <td>${data_in_json.data[i].destination}</td>
+                    <td>${data_in_json.data[i].payment}</td>
+                    <td>${data_in_json.data[i].status}</td>
+                </tr>`;
+            }
+                }
+            divToInsert += `</tbody>
+    </table>
+    <button type="button" id="confirm-btn">Confirm</button>
+        <button type="button" id="delete-btn">Delete</button>`
+            document.getElementById("tableData").innerHTML = divToInsert;
+            } 
+        else{
+            console.log("api returned false for success, printing the log");
+            console.log(data_from_json);
+            
+            // we can redirect to error page here
+            //window.location.replace("error.html");
+         }
+    })
+    .catch(err => console.log(err))
+
 }
