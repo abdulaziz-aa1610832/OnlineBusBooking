1ce8baf1705f27eabbd9753a05f68379bdd7b9b2
Tareq Al-Ahdal <TareqOAlAhdal@gmail.com>
2020-07-08 18:29:56 +0800

    login functionality with validation is complete

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,7 @@
 // JS functions for the main page
 
 document.getElementById("loginBtn")
-    .addEventListener('click', validateLogin); //doLogin originally
+    .addEventListener('click', doLogin);
 
 document.getElementById("signupbtn")
     .addEventListener('click', changeToSignUp);
@@ -54,13 +54,19 @@ function getSession() {
 function validateLogin() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
+    let canLogin = true;
+    document.getElementById("usernameLoginError").innerHTML = "";
+    document.getElementById("passwordLoginError").innerHTML = "";
 
     if (username == "") {
         document.getElementById("usernameLoginError").innerHTML = "Username required*";
+        canLogin = false;
     }
     if (password == "") {
         document.getElementById("passwordLoginError").innerHTML = "Password required*";
+        canLogin = false;
     }
+    return canLogin;
 }
 
 function validateSignUp() {
@@ -69,22 +75,34 @@ function validateSignUp() {
     let phoneNo = document.getElementById("phoneNo").value;
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
+    let canRegister = true;
+    document.getElementById("fullNameSignUpError").innerHTML = "";
+    document.getElementById("emailSignUpError").innerHTML = "";
+    document.getElementById("phoneNoSignUpError").innerHTML = "";
+    document.getElementById("usernameSignUpError").innerHTML = "";
+    document.getElementById("passwordSignUpError").innerHTML = "";
 
     if (fullName == "") {
         document.getElementById("fullNameSignUpError").innerHTML = "Full name required*";
+        canRegister = false;
     }
     if (email == "") {
         document.getElementById("emailSignUpError").innerHTML = "Email required*";
+        canRegister = false;
     }
     if (phoneNo == "") {
         document.getElementById("phoneNoSignUpError").innerHTML = "Phone number required*";
+        canRegister = false;
     }
     if (username == "") {
         document.getElementById("usernameSignUpError").innerHTML = "Username required*";
+        canRegister = false;
     }
     if (password == "") {
         document.getElementById("passwordSignUpError").innerHTML = "Password required*";
+        canRegister = false;
     }
+    return canRegister;
 }
 
 function changeToSignUp() {
@@ -98,29 +116,29 @@ function changeToSignUp() {
    <div id="form-container">
        <div>
            <div class="input-container">
-               <input type="text" name="fullName" id="fullName" required>
+               <input type="text" name="fullName" id="fullName">
                <label>Full Name</label>
                <p id="fullNameSignUpError"></p>
            </div>
            <div class="input-container">
-               <input type="email" name="email" id="email" required>
+               <input type="email" name="email" id="email">
                <label>Email</label>
                <p id="emailSignUpError"></p>
            </div>
-           <div class="input-container"><input type="text" name="phoneNo" id="phoneNo" pattern="01[0-9]{8}"
-                   title="Format: 01xxxxxxxx (Length: 10 digits)" required>
+           <div class="input-container">
+               <input type="text" name="phoneNo" id="phoneNo">
                <label>Phone Number</label>
                <p id="phoneNoSignUpError"></p>
            </div>
        </div>
        <div>
            <div class="input-container">
-               <input type="text" name="username" id="username" required>
+               <input type="text" name="username" id="username">
                <label>Username</label>
                <p id="usernameSignUpError"></p>
            </div>
            <div class="input-container" id="passwordSection">
-               <input type="password" name="password" id="password" required>
+               <input type="password" name="password" id="password">
                <label>Password</label>
                <p id="passwordSignUpError"></p>
            </div>
@@ -128,7 +146,7 @@ function changeToSignUp() {
    </div>
    <div id="buttons">
    
-       <a class="btn" onclick="validateSignUp()" id="regbtn">Register</a>
+       <a class="btn" onclick="registerUser()" id="regbtn">Register</a>
        <div id="haveAnAccount">
            <a href="index.html">Have an account?</a>
        </div>
@@ -137,59 +155,58 @@ function changeToSignUp() {
   `
     document.getElementById("signup-box").innerHTML = newFormContent;
     document.getElementById("signup-box").style.display = "block";
-    //onclick="registerUser()" in the Register button previously in the dynamic html above
 }
 
 function doLogin(theEvent) {
 
+    if (validateLogin()) {
+        console.log("do login starts..");
+        //  going to prevent the page from reloading or
+        // navigating away when you actually submit the form
+        theEvent.preventDefault();
 
-    console.log("do login starts..");
-    //  going to prevent the page from reloading or
-    // navigating away when you actually submit the form
-    theEvent.preventDefault();
+        // extract data from the form.
+        let userFromForm = document.getElementById("username").value;
+        let passwordFromForm = document.getElementById("password").value;
 
-    // extract data from the form.
-    let userFromForm = document.getElementById("username").value;
-    let passwordFromForm = document.getElementById("password").value;
+        console.log("data from form...");
+        console.log(userFromForm);
+        console.log(passwordFromForm);
 
-    console.log("data from form...");
-    console.log(userFromForm);
-    console.log(passwordFromForm);
+        console.log("before fetch.. trying to fetch");
+        fetch('http://127.0.0.1/OnlineBusBooking/backend/api.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `action=doLogin&data={"username":"${userFromForm}", "password":"${passwordFromForm}"}`
+            })
+            .then((response_from_api) => response_from_api.json())
+            .then(data_from_json => {
+                console.log("test after response.. converting to json next");
 
-    console.log("before fetch.. trying to fetch");
-    fetch('http://127.0.0.1/OnlineBusBooking/backend/api.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `action=doLogin&data={"username":"${userFromForm}", "password":"${passwordFromForm}"}`
-        })
-        .then((response_from_api) => response_from_api.json())
-        .then(data_from_json => {
-            console.log("test after response.. converting to json next");
+                if (data_from_json.success) {
+                    console.log("success");
+                    console.log(data_from_json);
+                    if (data_from_json.data.level == "1") {
+                        console.log("admin user");
+                        window.location.href = "admin.html";
+                    } else {
+                        console.log("normal user");
+                        window.location.href = "dashboard.html";
+                    }
 
-            if (data_from_json.success) {
-                console.log("success");
-                console.log(data_from_json);
-                if (data_from_json.data.level == "1") {
-                    console.log("admin user");
-                    window.location.href = "admin.html";
+
                 } else {
-                    console.log("normal user");
-                    window.location.href = "dashboard.html";
+                    console.log("api returned false for success, printing the log");
+                    console.log(data_from_json);
+
+                    // we can redirect to error page here
+                    //window.location.replace("error.html");
                 }
-
-
-            } else {
-                console.log("api returned false for success, printing the log");
-                console.log(data_from_json);
-
-                // we can redirect to error page here
-                //window.location.replace("error.html");
-            }
-        })
-        .catch(err => console.log(err))
-
+            })
+            .catch(err => console.log(err))
+    } else {}
 }
 
 
